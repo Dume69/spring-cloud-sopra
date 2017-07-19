57ec86d093b667d04924d6a667a953fe92b77ca5
Olivier Ducarme <olivier.ducarme@soprabanking.com>
2017-07-19 17:09:31 +0200

    day 2

--- spring-boot-atelier-1/src/main/java/com/sopra/boot/atelier1/controller/InvoiceController.java
+++ spring-boot-atelier-1/src/main/java/com/sopra/boot/atelier1/controller/InvoiceController.java
@@ -2,10 +2,10 @@ package com.sopra.boot.atelier1.controller;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
 
 import com.sopra.boot.atelier1.model.Invoice;
 import com.sopra.boot.atelier1.service.InvoiceService;
@@ -14,6 +14,9 @@ import com.sopra.boot.atelier1.service.InvoiceService;
 @RequestMapping("/invoice")
 public class InvoiceController {
 	private InvoiceService invoiceService;
+	@Value("${instance.number}")
+	private String instanceId;
+	private static final Logger logger = LoggerFactory.getLogger(InvoiceController.class);
 
 	public InvoiceController(InvoiceService invoiceService) {
 		this.invoiceService = invoiceService;
@@ -22,7 +25,18 @@ public class InvoiceController {
 	@GetMapping
 	@ResponseBody
 	public List<Invoice> findAll() {
+		logger.info("----- Find all invoices on instance {} ------- ", instanceId);
+//		try {
+//			Thread.sleep(3000);
+//		} catch (InterruptedException e) {
+//			throw new RuntimeException(e);
+//		}
 		return invoiceService.findAll();
+	}
+
+	@PostMapping
+	public void createInvoice(Invoice invoice) {
+		invoiceService.save(invoice);
 	}
 
 }
---
+++ spring-boot-atelier-1/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO invoice VALUES (0, TO_DATE('06/06/2006', 'mm/dd/yyyy'));
+INSERT INTO invoice VALUES (1, TO_DATE('06/06/2016', 'mm/dd/yyyy'));
+INSERT INTO invoice VALUES (2, TO_DATE('06/06/2026', 'mm/dd/yyyy'))
